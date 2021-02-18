40f4c3250c7fa332d24117c02b6502d79ec4f2b3
PrystaikoVolodymyr <volodymyr.p445@gmail.com>
2021-02-18 18:01:15 +0200

    lesson1

---
+++ lesson1/dir/films/jFile.js
@@ -0,0 +1,16 @@
+const name=`Vova`
+const age=`19`
+
+function consoller(string) {
+    console.log(string)
+}
+
+function fullName(name,age) {
+    consoller(`Hello.. my name is ${name}...I have ${age}`)
+}
+ console.log(2222)
+module.exports={
+    name,
+    age,
+    fullName
+}
